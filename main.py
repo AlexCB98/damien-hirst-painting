d0b9b1db63ca43123a9a3339f1d4f03ac11e46af
Alexandru <lexyus22@gmail.com>
2026-05-17 12:30:03 +0200

    Draw first row of random colored dots.

--- main.py
+++ main.py
@@ -12,3 +12,29 @@ def random_color():
 
     return r, g, b
 
+c.speed('fastest')
+c.width(5)
+c.penup()
+c.goto(-700, -400)
+
+
+
+for col in range(18):
+    c.color(random_color())
+    c.pendown()
+    c.begin_fill()
+    c.circle(20)
+    c.end_fill()
+    c.penup()
+    c.forward(40)
+    c.forward(40)
+    c.pendown()
+
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
